6dba46a5ea95e3028646f5e0856fd28ff4dc8cc2
Kamlesh Kumar <kamlesh.kumar@agreeya.com>
2019-05-27 12:01:51 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pdf-example';
+
+  page: number = 1;
+  totalPages: number;
+  isLoaded: boolean = false;
+
+  afterLoadComplete(pdfData: any) {
+    this.totalPages = pdfData.numPages;
+    this.isLoaded = true;
+  }
+
+  nextPage() {
+    this.page++;
+  }
+
+  prevPage() {
+    this.page--;
+  }
 }
